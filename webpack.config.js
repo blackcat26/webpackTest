1d1cbdbc6ec6cf7c4a3303023b06befc97ad413a
blackcat26 <469167595@qq.com>
2017-07-06 14:25:01 +0800

    test5

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,16 @@ module.exports = {
 	module:{
 		loaders:[
 			{
-				test:/\.css$/,
-				loader:'style-loader!css-loader'
-			}
+				test:/\.(png|jpg)$/,
+				loader:'url-loader?limit=8192&name=src/img/[name].[ext]',
+			},
+			/*{
+				test: /\.(png|jpe?g|gif|ico)(\?\S*)?$/,
+				loader: 'file-loader',
+				query: {
+					name: 'src/img/public/[name].[ext]'
+				}
+			},*/
 		]
 	}
 }
